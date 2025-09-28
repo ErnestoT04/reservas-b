55f4c2112edad1687004d2b05c3de6bd406bbd57
ErnestoT04 <eernestoj21@gmail.com>
2025-09-28 12:30:03 -0600

    chore: Proyecto base generado con Spring Initializr

---
+++ src/test/java/sv/edu/catolica/Reservas/ReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.catolica.Reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
