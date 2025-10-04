47fe319c41b2db2fec096070173ad8006eb7ac51
LuisS-byte <luis.sarmiento@catolica.edu.sv>
2025-10-03 20:44:44 -0600

    feat(rol): convertir clase Rol en entidad JPA con Lombok
    agregué constructores, la entidad, y los getters y setters al modelo Rol

--- src/main/java/sv/edu/catolica/Reservas/model/Rol.java
+++ src/main/java/sv/edu/catolica/Reservas/model/Rol.java
@@ -1,10 +1,20 @@
 package sv.edu.catolica.Reservas.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@Entity
+@Table(name = "rol")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Rol {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
